e0e109ca1c2703bb5a42980bcb43bdc8226fd82a
Joey Malvinni <76851062+joeymalvinni@users.noreply.github.com>
2023-05-25 19:15:35 -0700

    Update Find the odd int.js

--- javascript/6kyu/Find the odd int.js
+++ javascript/6kyu/Find the odd int.js
@@ -1,10 +1,5 @@
- function findOdd(a) {
-  let result = 0;
-  
-  for (let i = 0; i < a.length; i++) {
-    // use XOR because an even amount of numbers in an array will cancel, leaving the number seen an odd amount of times
-    result ^= a[i];
-  }
-  
-  return result;
+// refactor to use reduce
+// use XOR because an even amount of numbers in an array will cancel, leaving the number seen an odd amount of times
+function findOdd(a) {
+  return a.reduce((n, r) => n ^= r, 0);
 }
